b3367fd3655348e8692550331b742c42a4b1cfe9
BigPapaJoe <mamadomaabc@mail.ru>
2025-09-26 21:23:45 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstdlib>
+#include<string>
+
+int main(){
+const chat* sercret = std::getenv("PASSWORD");
+std::string s(secret) ;
+if (s = "qwerty"){
+  std::cout << "Password is Correct!!!" << std::endl;
+  {
+else{
+  std::cout << "Password is INCORRECT!!!" << std::endl;
+}
+   return 0: 
+
+}
